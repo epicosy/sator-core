a9ba38077e8f7d44ac0431e2b8cfd81c57da3236
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2025-05-05 13:11:58 +0100

    updated extract_product_attributes in ProductAttributesExtraction use case to receive vendor and name as input; added ProductAttributesExtractorPort to perform the extraction from the references; added tests; bumped version;

--- sator_core/ports/driving/extraction/attributes/product.py
+++ sator_core/ports/driving/extraction/attributes/product.py
@@ -5,5 +5,5 @@ from sator_core.models.product.attributes import ProductAttributes
 
 class ProductAttributesExtractionPort(ABC):
     @abstractmethod
-    def extract_product_attributes(self, product_id: str) -> ProductAttributes | None:
+    def extract_product_attributes(self, vendor: str, name: str) -> ProductAttributes | None:
         raise NotImplementedError
